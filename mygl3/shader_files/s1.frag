ca395fc2a83c7e87a113af17d9b8b2497146b277
Oleksiy Grechnyev <shrike4625@gmail.com>
2016-12-12 14:56:57 +0200

    mygl3 works with files; mygl4 started

---
+++ mygl3/shader_files/s1.frag
@@ -0,0 +1,6 @@
+#version 330 core
+uniform vec4 vertexColor;
+out vec4 color;
+void main(){
+  color=vertexColor;
+}
--- mygl4/mygl4.c
+++ mygl4/mygl4.c
@@ -5,11 +5,10 @@
 
 // GLEW
 #define GLEW_STATIC
-/** GL3 by Oleksiy Grechnyev 
- *  Fun with shaders 
- *  Triangle 1 uses time-dependent color via uniform
- *  Triangle 2 uses xyzrgb vertices and interpolated color, it moves
-*
+/** GL4 by Oleksiy Grechnyev 
+ *  Fun with textures
+ *  In progress, unfinished !!!
+ * 
  *  This is based on the learnopengl.com tutorial
  *  But write everything by hand, using C (and not C++) and more modular structure
  */
@@ -60,22 +59,15 @@ int main(){
     GLuint shaderProgram1 =  createShaderProgram("shader_files/s1.vs", "shader_files/s1.frag");
     GLuint vertexColorLocation1 = glGetUniformLocation(shaderProgram1, "vertexColor");
 
-    // Triangle 2: xyzrgb vertices
-
-
-    GLuint shaderProgram2 =  createShaderProgram("shader_files/s2.vs", "shader_files/s2.frag");
-    GLuint xOffsetLocation2 = glGetUniformLocation(shaderProgram2, "xOffset");
-    GLuint yOffsetLocation2 = glGetUniformLocation(shaderProgram2, "yOffset");
-
     //----
     // Vertex data, VBO, VAO
  
 
     // Triangle 1: changes color
     GLfloat vertices1[] = {
-         -0.2f,  -0.3f, 0.0f,  
-         -0.8f,  -0.3f, 0.0f,  
-         -0.5f,   0.3f, 0.0f 
+         -0.5f,  -0.5f, 0.0f,  
+          0.5f,  -0.5f, 0.0f,  
+          0.0f,   0.5f, 0.0f 
     };
     // Indices
     GLuint indices1[]={
@@ -85,25 +77,10 @@ int main(){
     GLuint VAO1, VBO1, EBO1;
     createVAO(&VAO1, &VBO1, &EBO1, vertices1, sizeof(vertices1), indices1, sizeof(indices1),1);    
 
-    // Triangle 2 :xyzrgb verices
-
-
-    GLfloat vertices2[] = {
-          0.2f,  -0.3f, 0.0f,  1.0f, 0.0f, 0.0f,
-          0.8f,  -0.3f, 0.0f,  0.0f, 1.0f, 0.0f,
-          0.5f,   0.3f, 0.0f,  0.0f, 0.0f, 1.0f
-    };
-    // Indices
-    GLuint indices2[]={
-        0, 1, 2  // Triangle 
-    };
-
-    GLuint VAO2, VBO2, EBO2;
-    createVAO(&VAO2, &VBO2, &EBO2, vertices2, sizeof(vertices2), indices2, sizeof(indices2),2);    
 
     //----
     // Set Clear (background) color
-    glClearColor(randf(), randf(), randf(), 1.0f);
+    glClearColor(0.5f, 1.0f, 1.0f, 1.0f);
     
     // Uncomment this for wireframe mode
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -122,26 +99,10 @@ int main(){
         // Draw the triangles
 
         // Triangle 1 
-        // Set the color
-        GLfloat timeVal=(GLfloat)glfwGetTime();
-        GLfloat redVal=(sin(timeVal*sqrt(2.0f))/2)+0.5f;
-        GLfloat greenVal=(sin(timeVal)/2)+0.5f;
-        GLfloat blueVal=(sin(timeVal*sqrt(3.0f))/2)+0.5f;
-        glUseProgram(shaderProgram1);
-        glUniform4f(vertexColorLocation1, redVal, greenVal, blueVal, 1.0f);
-        
+        glUseProgram(shaderProgram1);        
 
         glBindVertexArray(VAO1);    
         glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-
- 
-        // Triangle 2
-        glUseProgram(shaderProgram2);
-        glUniform1f(xOffsetLocation2, sin(timeVal*sqrt(5.0f))*0.1f);
-        glUniform1f(yOffsetLocation2, sin(timeVal*sqrt(5.5f))*0.1f);
-
-        glBindVertexArray(VAO2);    
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0); 
         
         glBindVertexArray(0);
         
@@ -155,9 +116,6 @@ int main(){
     glDeleteVertexArrays(1, &VAO1);
     glDeleteBuffers(1, &VBO1);
     glDeleteBuffers(1, &EBO1);
-    glDeleteVertexArrays(1, &VAO2);
-    glDeleteBuffers(1, &VBO2);
-    glDeleteBuffers(1, &EBO2);
     
     // Finish the program
     glfwTerminate();
@@ -166,15 +124,8 @@ int main(){
 //-------------------------------------------------------------------------
 //Callback functions
 
+// Escape only, no funny stuff
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode){
-    if (action==GLFW_PRESS) {
-        printf("Press : %d\n", key);
-        
-        // Set random Clear color
-        glClearColor(randf(), randf(), randf(), 1.0f);        
-        
-    } else if (action==GLFW_RELEASE)
-        printf("Release : %d\n", key); 
         
     if (key==GLFW_KEY_ESCAPE && action==GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
